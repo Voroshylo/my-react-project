2dd7cd50381858048086ac91c8e72b0abc80ba42
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-06-27 21:09:49 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+// import App from './components/App.jsx'
 import './index.css'
+import App from './components/books.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
 )
